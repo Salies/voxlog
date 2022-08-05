8cbfbc5f728748fe8100a1e06edf00b30e421909
Becelli <gustavobecelli@gmail.com>
2022-08-05 20:41:44 -0300

    starting with TypeScript

---
+++ client/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
+
+function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+	Component: PropTypes.elementType.isRequired,
+	pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
--- client/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-//import Footer from '../src/components/footer';
-export default function Home() {
-  return (
-    <main>
-      Hello World!
-    </main>
-  );
-}
