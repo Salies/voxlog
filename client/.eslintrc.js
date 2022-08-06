baa42582eb096187431dbf3ff3e1096a0b4d1e42
Becelli <gustavobecelli@gmail.com>
2022-08-06 03:03:05 -0300

    fix: updating tools to the latest versions

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,23 @@
 /*global module*/
 /*eslint no-undef: "error"*/
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    indent: ['error', 'tab'],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['warn', 'unix'],
+		quotes: ['warn', 'single'],
+		semi: ['error', 'always'],
+	},
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,13 @@
+// Configure tailwind.css in the project root to override these defaults
 module.exports = {
 	purge: {
 		mode: 'all',
 		preserveHtmlElements: true,
-		content: [
-			'./pages/**/*.{js,jsx,ts,tsx}',
-			'./src/components/**/*.{js,jsx,ts,tsx}',
-		],
+		content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
 	},
-	darkMode: 'class', // or 'media' or 'class'
+	darkMode: 'class', // or 'media'
 	theme: {
 		extend: {},
-		container: {
-			center: true,
-		},
 	},
 	variants: {
 		extend: {},
