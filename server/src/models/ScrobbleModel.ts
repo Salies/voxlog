c3e7a66b145bbc43555ab4663dd2f974e8b8ca8c
Gustavo Becelli <gustavobecelli@gmail.com>
2022-10-22 14:02:38 -0300

    feat: creating a scrobble

--- server/src/models/ScrobbleModel.ts
+++ server/src/models/ScrobbleModel.ts
@@ -3,11 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default class ScrobbleModel {
-	get = async (scrobble: any) => {
-		return await prisma.scrobble.findUnique({
-			where: {
-				id: scrobble.id,
-			},
-		});
-	};
+	create = async () => {
+
+
+	// get = async (scrobble: any) => {
+	// 	return await prisma.scrobble.findUnique({
+	// 		where: {
+	// 			id: scrobble.id,
+	// 		},
+	// 	});
+	// };
 }
