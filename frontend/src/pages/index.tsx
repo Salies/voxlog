8544a778e7c177371651471d4118572b12b02368
becelli <gustavobecelli@gmail.com>
2023-01-02 15:00:30 -0300

    feat: new features

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,16 @@
-import Navbar from "../components/navbar";
+import Navbar from '../components/navbar'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <>
-      <div className="w-full h-screen bg-gradient-to-r from-pink-400 to-red-500 ">
-        <div className="flex items-center justify-center h-full">
-          <h1 className="text-3xl font-bold md:text-6xl">
-            ⚠️ Work in progress ⚠️
-          </h1>
-        </div>
-      </div>
-    </>
-  );
+    // citar, drums, drums_2, guitar, guitar_2, piano, sax
+    <div className="h-screen w-full md:grid md:grid-cols-3">
+      <Image src="/instruments_guitar_2.png" width={1000} height={1000} alt="guitar_2" />
+      <Image src="/instruments_drums.png" width={1000} height={1000} alt="drums" />
+      <Image src="/instruments_sax.png" width={1000} height={1000} alt="sax" />
+      <Image src="/instruments_citar.png" width={1000} height={1000} alt="citar" />
+      {/* <Image src="/instruments_drums_2.png" width={1000} height={1000} alt="drums_2" /> */}
+      <Image src="/instruments_piano.png" width={1000} height={1000} alt="piano" />
+      <Image src="/instruments_guitar.png" width={1000} height={1000} alt="guitar" />
+    </div>
+  )
 }
