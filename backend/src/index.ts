e322447e330f58dd16a3eba7670e2cd57347a79f
Becelli <gustavobecelli@gmail.com>
2023-01-10 16:28:47 -0300

    using cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,15 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import scrobblesRoutes from './scrobbles/routes';
 import userRoutes from './users/routes';
 import trackRoutes from './tracks/routes';
 import albumRoutes from './albums/routes';
 import artistRoutes from './artists/routes';
-import { DateTime } from 'luxon';
-import { cors } from './utils/cors';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => cors(req, res, next));
+app.use(cors());
 
 app.use('/scrobbles', scrobblesRoutes);
 app.use('/users', userRoutes);
